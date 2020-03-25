4c1e1927dee22c3ea18588b7ec2e5b28c89f4373
José María Signanini <josesigna@gmail.com>
2020-03-24 21:57:04 -0400

    Improve test coverage for MarshalErrors.

--- marshal_errors_test.go
+++ marshal_errors_test.go
@@ -6,28 +6,131 @@ import (
 )
 
 func TestMarshalErrors(t *testing.T) {
-	e := Error{
-		ID:     "someID",
+	// simple error
+	simpleError := Error{
+		ID:     "NOT_FOUND",
 		Status: "404",
 		Title:  "not-found",
 	}
-	expected := []byte(`{
+	simpleErrorExpected := []byte(`{
 	"jsonapi": {
 		"version": "1.0"
 	},
 	"errors": [
 		{
-			"id": "someID",
+			"id": "NOT_FOUND",
 			"status": "404",
 			"title": "not-found"
 		}
 	]
 }`)
-	if b, err := MarshalErrors(nil, e); err != nil {
+	if b, err := MarshalErrors(nil, simpleError); err != nil {
 		t.Errorf(err.Error())
 	} else {
-		if bytes.Compare(expected, b) != 0 {
-			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(expected), string(b))
+		if bytes.Compare(simpleErrorExpected, b) != 0 {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(simpleErrorExpected), string(b))
+		}
+	}
+
+	// error with links
+	simpleErrorWithLinks := Error{
+		ID:     "NOT_FOUND",
+		Status: "404",
+		Title:  "not-found",
+		Links: Links{
+			"about": "/errors/NOT_FOUND",
+		},
+	}
+	simpleErrorWithLinksExpected := []byte(`{
+	"jsonapi": {
+		"version": "1.0"
+	},
+	"errors": [
+		{
+			"id": "NOT_FOUND",
+			"links": {
+				"about": "/errors/NOT_FOUND"
+			},
+			"status": "404",
+			"title": "not-found"
+		}
+	]
+}`)
+	if b, err := MarshalErrors(nil, simpleErrorWithLinks); err != nil {
+		t.Errorf(err.Error())
+	} else {
+		if bytes.Compare(simpleErrorWithLinksExpected, b) != 0 {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(simpleErrorWithLinksExpected), string(b))
+		}
+	}
+
+	// error with meta
+	simpleErrorWithMeta := Error{
+		ID:     "NOT_FOUND",
+		Status: "404",
+		Title:  "not-found",
+		Meta: Meta{
+			"errors_documentation_url": "https://example.com",
+		},
+	}
+	simpleErrorWithMetaExpected := []byte(`{
+	"jsonapi": {
+		"version": "1.0"
+	},
+	"errors": [
+		{
+			"id": "NOT_FOUND",
+			"status": "404",
+			"title": "not-found",
+			"meta": {
+				"errors_documentation_url": "https://example.com"
+			}
+		}
+	]
+}`)
+	if b, err := MarshalErrors(nil, simpleErrorWithMeta); err != nil {
+		t.Errorf(err.Error())
+	} else {
+		if bytes.Compare(simpleErrorWithMetaExpected, b) != 0 {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(simpleErrorWithMetaExpected), string(b))
+		}
+	}
+
+	// error with meta and links
+	simpleErrorWithMetaAndLinks := Error{
+		ID:     "NOT_FOUND",
+		Status: "404",
+		Title:  "not-found",
+		Links: Links{
+			"about": "/errors/NOT_FOUND",
+		},
+		Meta: Meta{
+			"errors_documentation_url": "https://example.com",
+		},
+	}
+	simpleErrorWithMetaAndLinksExpected := []byte(`{
+	"jsonapi": {
+		"version": "1.0"
+	},
+	"errors": [
+		{
+			"id": "NOT_FOUND",
+			"links": {
+				"about": "/errors/NOT_FOUND"
+			},
+			"status": "404",
+			"title": "not-found",
+			"meta": {
+				"errors_documentation_url": "https://example.com"
+			}
+		}
+	]
+}`)
+	if b, err := MarshalErrors(nil, simpleErrorWithMetaAndLinks); err != nil {
+		t.Errorf(err.Error())
+	} else {
+		if bytes.Compare(simpleErrorWithMetaAndLinksExpected, b) != 0 {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(simpleErrorWithMetaAndLinksExpected), string(b))
 		}
 	}
 }
