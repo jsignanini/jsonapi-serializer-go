9ad0a9bd64586d6e7549aacd2a4aba0269c00aa1
José María Signanini <josesigna@gmail.com>
2020-03-24 22:19:19 -0400

    Add SetTagKey test.

--- jsonapi_test.go
+++ jsonapi_test.go
@@ -1,6 +1,9 @@
 package jsonapi
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestSetJSONPrefix(t *testing.T) {
 	def := ""
@@ -29,5 +32,34 @@ func TestSetJSONIndent(t *testing.T) {
 }
 
 func TestSetTagKey(t *testing.T) {
-	// TODO
+	SetTagKey("customKey")
+	type Article struct {
+		ID    string `customKey:"primary,articles"`
+		Title string `customKey:"attribute,title"`
+	}
+	article := Article{
+		ID:    "article-id",
+		Title: "Hello World!",
+	}
+	articleExpected := []byte(`{
+	"data": {
+		"id": "article-id",
+		"type": "articles",
+		"attributes": {
+			"title": "Hello World!"
+		}
+	},
+	"jsonapi": {
+		"version": "1.0"
+	}
+}`)
+	if got, err := Marshal(&article, nil); err != nil {
+		t.Errorf(err.Error())
+	} else {
+		if bytes.Compare(got, articleExpected) != 0 {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", string(articleExpected), string(got))
+		}
+	}
+	// TODO fix this so we don't have to reset the key
+	SetTagKey("jsonapi")
 }
