08a69f32ccd886c8dc067c173dbd9552c597b5a5
José María Signanini <josesigna@gmail.com>
2020-05-08 13:39:12 -0400

    Add HTTP response handlers.

---
+++ respond.go
@@ -0,0 +1,33 @@
+package jsonapi
+
+import (
+	"net/http"
+)
+
+// Respond
+func Respond(w http.ResponseWriter, r *http.Request, statusCode int, v interface{}) error {
+	w.Header().Set("Content-Type", ContentType)
+	body, err := Marshal(v, nil)
+	if err != nil {
+		return err
+	}
+	return respond(w, r, statusCode, body)
+}
+
+// RespondError
+func RespondError(w http.ResponseWriter, r *http.Request, statusCode int, p *MarshalParams, errs ...Error) error {
+	w.Header().Set("Content-Type", ContentType)
+	body, err := MarshalErrors(p, errs...)
+	if err != nil {
+		return err
+	}
+	return respond(w, r, statusCode, body)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, statusCode int, body []byte) error {
+	w.WriteHeader(statusCode)
+	if _, err := w.Write(body); err != nil {
+		return err
+	}
+	return nil
+}
